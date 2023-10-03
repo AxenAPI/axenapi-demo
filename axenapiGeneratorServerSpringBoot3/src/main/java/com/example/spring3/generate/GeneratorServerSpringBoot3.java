c37ca68ff07c3e166f790a6a65edd7963b6531e0
alexandra.volushkova <alexandra.volushkova@ax-team.com>
2023-10-03 18:55:28 +0300

    rename app classes

--- axenapiGeneratorServerSpringBoot3/src/main/java/com/example/spring3/generate/GeneratorServerSpringBoot3.java
+++ axenapiGeneratorServerSpringBoot3/src/main/java/com/example/spring3/generate/GeneratorServerSpringBoot3.java
@@ -2,16 +2,14 @@ package com.example.spring3.generate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.kafka.annotation.EnableKafka;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableKafka
-public class DemoSpring3GenerateApplication {
+public class GeneratorServerSpringBoot3 {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoSpring3GenerateApplication.class, args);
+        SpringApplication.run(GeneratorServerSpringBoot3.class, args);
     }
 
 }
